0a85007c845b2525b00a9fa421a100f9b39c6067
Hongsik Choi <playhong16@gmail.com>
2024-01-07 15:59:13 +0900

    [feat]: NetworkManager - API 통신 및 JSON 데이터 파싱 메서드 구현

--- FestivalApp/Sources/Manager/NetworkManager.swift
+++ FestivalApp/Sources/Manager/NetworkManager.swift
@@ -13,6 +13,7 @@ final class NetworkManager {
     
     private init() {}
     
+    let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
     let session = URLSession(configuration: URLSessionConfiguration.default)
     
     func fetchImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
@@ -20,13 +21,35 @@ final class NetworkManager {
         let task = session.dataTask(with: url) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse,
                   (200..<300).contains(httpResponse.statusCode) else {
-                print("ERROR: NetworkManager - fetchImage(urlString:completion) HTTP 응답 코드는 - \(response) 입니다.")
+                print("NetworkManager - ERROR: fetchImage(urlString:completion) 의 HTTP 응답 코드는 - \(response) 입니다.")
                 return
             }
 
             guard let data = data else { return }
             let result = UIImage(data: data)
             completion(result)
+        }
+        task.resume()
+    }
+    
+    func fetchFestival(urlString: String, completion: @escaping ([Item]) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let task = session.dataTask(with: url) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                print("NetworkManager - ERROR: fetchFestival(urlString:completion) 의 HTTP 응답 코드는 - \(response) 입니다.")
+                return
+            }
+
+            guard let data = data else { return }
+            print(String(decoding: data, as: UTF8.self))
+            
+            do {
+                let decoder = JSONDecoder()
+                let welcome = try decoder.decode(Welcome.self, from: data)
+            } catch {
+                print("NetworkManager - ERROR: fetchFestival(urlString:completion) \(error)")
+            }
         }
         task.resume()
     }
