cc1e4a97f90723e07f0d3134997ae9a22b81665c
Hongsik Choi <playhong16@gmail.com>
2024-01-09 16:28:50 +0900

    [mod]: url의 base를 프로퍼티로 만들어서 사용하도록 변경

--- FestivalApp/Sources/Manager/NetworkManager.swift
+++ FestivalApp/Sources/Manager/NetworkManager.swift
@@ -13,8 +13,9 @@ final class NetworkManager {
     
     private init() {}
     
-    let session = URLSession(configuration: URLSessionConfiguration.default)
+    private let session = URLSession(configuration: URLSessionConfiguration.default)
     private let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String ?? ""
+    private let base = "https://apis.data.go.kr/B551011/KorService1/"
     
     func fetchImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
         guard let url = URL(string: urlString) else { return }
@@ -37,11 +38,9 @@ final class NetworkManager {
         let dateFormmatter = DateFormatter()
         dateFormmatter.dateFormat = "YYYYMMDD"
         let todayString = dateFormmatter.string(from: date)
-        print(todayString)
-        let base = "https://apis.data.go.kr/B551011/KorService1/"
         let path = "searchFestival1?numOfRows=10&MobileOS=IOS&MobileApp=FestivalApp&_type=json&eventStartDate=\(todayString)&serviceKey="
         
-        guard let url = URL(string: base + path + self.apiKey) else { return }
+        guard let url = URL(string: base + path + apiKey) else { return }
         let task = session.dataTask(with: url) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse,
                   (200..<300).contains(httpResponse.statusCode) else {
@@ -65,8 +64,8 @@ final class NetworkManager {
     }
     
     func fetchDetailInfomation(contentID: String, completion: @escaping (Information?) -> Void) {
-        let urlString = "https://apis.data.go.kr/B551011/KorService1/detailInfo1?MobileOS=IOS&MobileApp=FestivalApp&_type=json&contentId=\(contentID)&contentTypeId=15&serviceKey="
-        guard let url = URL(string: urlString + apiKey) else { return }
+        let path = "detailInfo1?MobileOS=IOS&MobileApp=FestivalApp&_type=json&contentId=\(contentID)&contentTypeId=15&serviceKey="
+        guard let url = URL(string: base + path + apiKey) else { return }
         let task = session.dataTask(with: url) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse,
                   (200..<300).contains(httpResponse.statusCode) else {
