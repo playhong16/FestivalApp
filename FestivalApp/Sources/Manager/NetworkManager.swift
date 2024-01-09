e6d653d6a3bc5a98a6531562906bae04f50d63e1
Hongsik Choi <playhong16@gmail.com>
2024-01-09 16:25:56 +0900

    [mod]: 오늘 날짜 기준으로 진행중인 축제를 가져오도록 fetchFestival(comletion:) 메서드 수정

--- FestivalApp/Sources/Manager/NetworkManager.swift
+++ FestivalApp/Sources/Manager/NetworkManager.swift
@@ -15,8 +15,6 @@ final class NetworkManager {
     
     let session = URLSession(configuration: URLSessionConfiguration.default)
     private let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String ?? ""
-    private let urlString = "https://apis.data.go.kr/B551011/KorService1/searchFestival1?numOfRows=10&MobileOS=IOS&MobileApp=FestivalApp&_type=json&eventStartDate=20230103&serviceKey="
-    
     
     func fetchImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
         guard let url = URL(string: urlString) else { return }
@@ -35,7 +33,15 @@ final class NetworkManager {
     }
     
     func fetchFestival(completion: @escaping ([Festival]) -> Void) {
-        guard let url = URL(string: self.urlString + self.apiKey) else { return }
+        let date = Date()
+        let dateFormmatter = DateFormatter()
+        dateFormmatter.dateFormat = "YYYYMMDD"
+        let todayString = dateFormmatter.string(from: date)
+        print(todayString)
+        let base = "https://apis.data.go.kr/B551011/KorService1/"
+        let path = "searchFestival1?numOfRows=10&MobileOS=IOS&MobileApp=FestivalApp&_type=json&eventStartDate=\(todayString)&serviceKey="
+        
+        guard let url = URL(string: base + path + self.apiKey) else { return }
         let task = session.dataTask(with: url) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse,
                   (200..<300).contains(httpResponse.statusCode) else {
