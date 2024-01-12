4f134dc75e78d86006b2365fb7074b1db3aeb6ba
Hongsik Choi <playhong16@gmail.com>
2024-01-13 01:21:52 +0900

    [mod]: 불필요한 데이터 사용하지 않도록 주석 처리

--- FestivalApp/Sources/Models/Festival.swift
+++ FestivalApp/Sources/Models/Festival.swift
@@ -40,9 +40,9 @@ struct Festival: Codable {
     let addr1: String
     let addr2: String
     let booktour: String
-    let cat1: Cat1
-    let cat2: Cat2
-    let cat3: Cat3
+//    let cat1: Cat1
+//    let cat2: Cat2
+//    let cat3: Cat3
     let contentid: String
     let contenttypeid: String
     let createdtime: String
@@ -61,7 +61,8 @@ struct Festival: Codable {
     let title: String
 
     enum CodingKeys: String, CodingKey {
-        case addr1, addr2, booktour, cat1, cat2, cat3, contentid, contenttypeid, createdtime, firstimage2
+        case addr1, addr2, booktour, contentid, contenttypeid, createdtime, firstimage2
+        // cat1, cat2, cat3
         case eventStartDate = "eventstartdate"
         case eventEndDate = "eventenddate"
         case imageURLString = "firstimage"
@@ -70,19 +71,19 @@ struct Festival: Codable {
     }
 }
 
-enum Cat1: String, Codable {
-    case a02 = "A02"
-}
-
-enum Cat2: String, Codable {
-    case a0207 = "A0207"
-    case a0208 = "A0208"
-}
-
-enum Cat3: String, Codable {
-    case a02070200 = "A02070200"
-    case a02081300 = "A02081300"
-}
+//enum Cat1: String, Codable {
+//    case a02 = "A02"
+//}
+//
+//enum Cat2: String, Codable {
+//    case a0207 = "A0207"
+//    case a0208 = "A0208"
+//}
+//
+//enum Cat3: String, Codable {
+//    case a02070200 = "A02070200"
+//    case a02081300 = "A02081300"
+//}
 
 enum CpyrhtDivCD: String, Codable {
     case empty = ""
