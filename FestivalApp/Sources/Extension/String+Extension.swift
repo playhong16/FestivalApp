23e4c10883b5aea96857cdea79288eb8b07ac230
Hongsik Choi <playhong16@gmail.com>
2024-01-12 15:21:41 +0900

    [mod]: 기존에 StringFormatter 의 타입 메서드로 컨텐츠 날짜의 문자열을 변경했는데, String을 확장해서 사용하도록 변경

--- FestivalApp/Sources/Extension/String+Extension.swift
+++ FestivalApp/Sources/Extension/String+Extension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension String {
+    func convertContentDate() -> String {
+        var convertedString = self
+        convertedString.insert(".", at: self.index(self.startIndex, offsetBy: 4))
+        convertedString.insert(".", at: self.index(self.startIndex, offsetBy: 7))
+        return convertedString
+    }
+}
