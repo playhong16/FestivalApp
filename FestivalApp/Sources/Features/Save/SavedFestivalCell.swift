1c1bf3a79155f0e0c2feaf737026f1e08280b64a
Hongsik Choi <playhong16@gmail.com>
2024-01-15 15:08:01 +0900

    [add]: 축제 저장 기능 구현을 위해 생성

---
+++ FestivalApp/Sources/Features/Save/SavedFestivalCell.swift
@@ -0,0 +1,12 @@
+//
+//  SavedFestivalCell.swift
+//  FestivalApp
+//
+//  Created by playhong on 2024/01/15.
+//
+
+import UIKit
+
+class SavedFestivalCell: UICollectionViewCell {
+    
+}
---
+++ FestivalApp/Sources/Features/Save/SavedFestivalViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SavedFestivalViewController.swift
+//  FestivalApp
+//
+//  Created by playhong on 2024/01/15.
+//
+
+import UIKit
+
+class SavedFestivalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
