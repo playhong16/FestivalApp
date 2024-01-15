9c6f0d70202214ebf8d92aa64bd88f6fddd45a65
Hongsik Choi <playhong16@gmail.com>
2024-01-15 14:40:39 +0900

    [feat]: 탭바 기본 구현

---
+++ FestivalApp/Sources/Features/TabBar/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  FestivalApp
+//
+//  Created by playhong on 2024/01/03.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        setupItems()
+    }
+    
+    private func configure() {
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .systemBlue
+    }
+    
+    private func setupItems() {
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        searchVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        self.viewControllers = [searchVC]
+    }
+}
