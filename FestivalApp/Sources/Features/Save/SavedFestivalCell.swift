1d4e3bd9ccc49b31dc5ec5d44bf72773064d6fb7
Hongsik Choi <playhong16@gmail.com>
2024-01-15 15:38:42 +0900

    [feat]: 컬렉션 뷰 기본 구현

--- FestivalApp/Sources/Features/Save/SavedFestivalCell.swift
+++ FestivalApp/Sources/Features/Save/SavedFestivalCell.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 class SavedFestivalCell: UICollectionViewCell {
+    static let identifier = "SavedFestivalCell"
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.backgroundColor = .gray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- FestivalApp/Sources/Features/Save/SavedFestivalViewController.swift
+++ FestivalApp/Sources/Features/Save/SavedFestivalViewController.swift
@@ -7,9 +7,63 @@
 
 import UIKit
 
-class SavedFestivalViewController: UIViewController {
+final class SavedFestivalViewController: UIViewController {
+    
+    // MARK: - Components
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.dataSource = self
+        cv.delegate = self
+        cv.register(SavedFestivalCell.self, forCellWithReuseIdentifier: SavedFestivalCell.identifier)
+        return cv
+    }()
+
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .brown
+        addSubviews()
+        setLayout()
+    }
+    
+    // MARK: - Layout
+
+    func addSubviews() {
+        view.addSubview(collectionView)
+    }
+    
+    func setLayout() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Extension
+
+extension SavedFestivalViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SavedFestivalCell.identifier, for: indexPath) as? SavedFestivalCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension SavedFestivalViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height / 5)
     }
 }
