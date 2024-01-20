9497f60d9f5a75c46dfb8d3671793dce3dcb6a52
Hongsik Choi <playhong16@gmail.com>
2024-01-20 23:06:19 +0900

    [feat]: 하늘색 추가

---
+++ FestivalApp/Sources/Extension/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  FestivalApp
+//
+//  Created by playhong on 2024/01/20.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customSky = UIColor(red: 173/255, green: 232/255, blue: 244/255, alpha: 1)
+}
