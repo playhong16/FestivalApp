a4504dc139bb77844e9be4203c3934853ca68365
Hongsik Choi <playhong16@gmail.com>
2024-02-02 17:11:59 +0900

    [chore]: 불필요한 데이터 사용하지 않도록 제거

--- FestivalApp/Sources/Models/Festival.swift
+++ FestivalApp/Sources/Models/Festival.swift
@@ -40,9 +40,6 @@ struct Festival: Codable {
     let addr1: String
     let addr2: String
     let booktour: String
-//    let cat1: Cat1
-//    let cat2: Cat2
-//    let cat3: Cat3
     let contentid: String
     let contenttypeid: String
     let createdtime: String
@@ -50,7 +47,6 @@ struct Festival: Codable {
     let eventEndDate: String
     let imageURLString: String
     let firstimage2: String
-    let cpyrhtDivCD: CpyrhtDivCD
     let mapx: String
     let mapy: String
     let mlevel: String
@@ -62,32 +58,11 @@ struct Festival: Codable {
 
     enum CodingKeys: String, CodingKey {
         case addr1, addr2, booktour, contentid, contenttypeid, createdtime, firstimage2
-        // cat1, cat2, cat3
         case eventStartDate = "eventstartdate"
         case eventEndDate = "eventenddate"
         case imageURLString = "firstimage"
-        case cpyrhtDivCD = "cpyrhtDivCd"
         case mapx, mapy, mlevel, modifiedtime, areacode, sigungucode, tel, title
     }
-}
-
-//enum Cat1: String, Codable {
-//    case a02 = "A02"
-//}
-//
-//enum Cat2: String, Codable {
-//    case a0207 = "A0207"
-//    case a0208 = "A0208"
-//}
-//
-//enum Cat3: String, Codable {
-//    case a02070200 = "A02070200"
-//    case a02081300 = "A02081300"
-//}
-
-enum CpyrhtDivCD: String, Codable {
-    case empty = ""
-    case type3 = "Type3"
 }
 
 // MARK: - Header
