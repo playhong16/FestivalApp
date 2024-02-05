6e9f0767c39a7cb77d05686469faedd3433f377c
Hongsik Choi <playhong16@gmail.com>
2024-02-05 15:25:18 +0900

    style: 스플래시 뷰 구현으로 인한 초기 루트 뷰 변경

--- FestivalApp/Sources/App/SceneDelegate.swift
+++ FestivalApp/Sources/App/SceneDelegate.swift
@@ -14,7 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MainTabBarController()
+        let storyBoard = UIStoryboard(name: "Splash", bundle: nil)
+        let splashViewController = storyBoard.instantiateInitialViewController()
+        window.rootViewController = splashViewController
         window.makeKeyAndVisible()
         window.overrideUserInterfaceStyle = .light
         self.window = window
