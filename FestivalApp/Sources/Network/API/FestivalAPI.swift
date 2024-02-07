870e85ab8dd44e45585fc3a206c698de50ba67c0
Hongsik Choi <playhong16@gmail.com>
2024-02-08 00:04:56 +0900

    create: FestivalAPI.swift - 축제 데이터를 전달받을 수 있도록 API EndPoint를 구현합니다.

--- FestivalApp/Sources/Network/API/FestivalAPI.swift
+++ FestivalApp/Sources/Network/API/FestivalAPI.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct FestivalAPI: API {
+    typealias ResponseType = FestivalWelcome
+    var configuration: APIConfiguration
+    
+    init(eventStartDate: String) {
+        self.configuration = APIConfiguration(baseURL: APIResource.baseURL,
+                                              path: "/B551011/KorService1/searchFestival1",
+                                              httpMethod: .get,
+                                              parameters: [
+                                                 "MobileOS": "IOS",
+                                                 "MobileApp": "FestivalAPP",
+                                                 "_type": "json",
+                                                 "serviceKey": APIResource.apiKey,
+                                                 "numOfRows": "20",
+                                                 "eventStartDate": eventStartDate
+                                              ])
+    }
+}
