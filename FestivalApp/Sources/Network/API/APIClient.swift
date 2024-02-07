2091572b06011076ccf559b1bfd8a2a94a8643ac
Hongsik Choi <playhong16@gmail.com>
2024-02-07 23:58:01 +0900

    create: APIClient.swift - API에 네트워크 요청을 보내는 역할을 합니다.

--- FestivalApp/Sources/Network/API/APIClient.swift
+++ FestivalApp/Sources/Network/API/APIClient.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct APIClient {
+    static let shared = APIClient(session: URLSession.shared)
+    
+    typealias RequestResult = (Result<Data, APIError>) -> Void
+    
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func reqeust(_ request: URLRequest?, completionHandler: @escaping RequestResult) {
+        guard let request = request else {
+            completionHandler(.failure(APIError.noRequest))
+            return
+        }
+        
+        session.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                completionHandler(.failure(APIError.noData))
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completionHandler(.success(data))
+            }
+        }.resume()
+    }
+}
