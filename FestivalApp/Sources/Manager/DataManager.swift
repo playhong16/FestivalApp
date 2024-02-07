64ae474034ea63d718185f511e3267872d1ed60c
Hongsik Choi <playhong16@gmail.com>
2024-02-08 00:11:24 +0900

    refactor: 1. 불필요한 코드를 제거합니다.

--- FestivalApp/Sources/Manager/DataManager.swift
+++ FestivalApp/Sources/Manager/DataManager.swift
@@ -16,35 +16,7 @@ final class DataManager {
     }
     private var _savedFestivals: [Festival] = []
     
-    private let networkManager = NetworkManager.shared
-    
     private init() {}
-    
-    func fetchFestivalsFromAPI(pageNumber: Int, completion: @escaping (Int, [Festival]) -> Void) {
-        networkManager.requestFestivals(pageNumber: pageNumber) { result in
-            switch result {
-            case .success(let welcomeData):
-                let nextPageNumber = welcomeData.response.body.pageNo + 1
-                let festivals = welcomeData.response.body.items.festivals
-                completion(nextPageNumber, festivals)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func fetchFestivalDetailInfomationFromAPI(contentID: String, completion: @escaping (Information) -> Void) {
-        networkManager.requestDetailInfomation(contentID: contentID) { result in
-            switch result {
-            case .success(let welcomeData):
-                if let information = welcomeData.response.body.items.information.first {
-                    completion(information)
-                }
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
     
     func saveFestival(_ festival: Festival) {
         _savedFestivals.append(festival)
