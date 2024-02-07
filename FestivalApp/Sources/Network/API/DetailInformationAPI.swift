a605233ad74e4fa1f1297864f34fc4ebb8e4f3fc
Hongsik Choi <playhong16@gmail.com>
2024-02-08 00:05:30 +0900

    create: DetailInformationAPI.swift - 축제 데이터의 상제 정보를 전달받을 수 있도록 API EndPoint를 구현합니다.

--- FestivalApp/Sources/Network/API/DetailInformationAPI.swift
+++ FestivalApp/Sources/Network/API/DetailInformationAPI.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct DetailInformationAPI: API {
+    typealias ResponseType = InformationWelcome
+    var configuration: APIConfiguration
+    
+    init(contentID: String) {
+        self.configuration = APIConfiguration(baseURL: APIResource.baseURL,
+                                              path: "/B551011/KorService1/detailInfo1",
+                                              httpMethod: .get,
+                                              parameters: [
+                                                "MobileOS": "IOS",
+                                                "MobileApp": "FestivalAPP",
+                                                "_type": "json",
+                                                "serviceKey": APIResource.apiKey,
+                                                "contentTypeId": "15",
+                                                "contentId": contentID
+                                              ])
+    }
+}
