c42f749042f5383b139afc71988ac857881e78a7
Hongsik Choi <playhong16@gmail.com>
2024-02-08 00:02:45 +0900

    create: API.swift - API를 사용하기 위해 준수해야하는 프로토콜입니다.

--- FestivalApp/Sources/Network/API/API.swift
+++ FestivalApp/Sources/Network/API/API.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol API {
+    associatedtype ResponseType: Decodable
+    typealias ResponseResult = (Result<ResponseType, APIError>) -> Void
+    var configuration: APIConfiguration { get }
+}
+
+extension API {
+    func execute(using client: APIClient = APIClient.shared,
+               completionHandler: @escaping ResponseResult) {
+        client.reqeust(configuration.urlRequest) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let decodedData = try decoder.decode(ResponseType.self, from: data)
+                    completionHandler(.success(decodedData))
+                } catch {
+                    completionHandler(.failure(.jsonDecodingError))
+                }
+            case .failure:
+                completionHandler(.failure(.noData))
+            }
+        }
+    }
+}
